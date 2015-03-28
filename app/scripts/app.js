62e32ad8c895563d8216af1a20a9877ccfaddb28
Lauriane Kayungu-N <lauriane.kayungu@gmail.com>
2015-03-28 12:36:52 +0000

    initalising project

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,4 +14,27 @@ angular
     'ngCookies',
     'ngSanitize',
     'ngTouch'
-  ]);
+  ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise('/');
+    //
+    // Now set up the states
+    $stateProvider
+      .state('dashboard', {
+        url: '/',
+        templateUrl: '',
+        controller: ''
+      })
+      .state('auth', {
+        url: '/login',
+        templateUrl: '',
+        controller: ''
+      })
+      .state('booking', {
+        url: '/booking',
+        templateUrl: '',
+        controller: ''
+      });
+  })
+;
--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -2,13 +2,13 @@
 
 /**
  * @ngdoc function
- * @name pilotPresenceApp.controller:MainCtrl
+ * @name pilotPresenceApp.controller:DashboardCtrl
  * @description
- * # MainCtrl
+ * # DashboardCtrl
  * Controller of the pilotPresenceApp
  */
 angular.module('pilotPresenceApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('DashboardCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
