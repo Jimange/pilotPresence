3e8c21e2cd0d42b31c2b46d5489c9114b3e9da0e
Lauriane Kayungu-N <lauriane.kayungu@gmail.com>
2015-03-28 12:01:16 +0000

    started pilot presence project

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pilotPresenceApp
+ * @description
+ * # pilotPresenceApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pilotPresenceApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pilotPresenceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pilotPresenceApp
+ */
+angular.module('pilotPresenceApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
