6a77a59c285bb4a2393f24baba3bcb20d57685d8
Lauriane Kayungu-N <lauriane.kayungu@gmail.com>
2015-03-29 08:53:07 +0100

    got the day calenday to work

--- app/scripts/controllers/calendar.js
+++ app/scripts/controllers/calendar.js
@@ -8,7 +8,7 @@
  * Controller of the pilotPresenceApp
  */
 angular.module('pilotPresenceApp')
-  .controller('CalendarCtrl', function ($scope, ratioClass) {
+  .controller('CalendarCtrl', function ($scope, ratioClass, $state, $filter, $stateParams) {
     $scope.$emit('calendarClass', ratioClass);
 
     $scope.calendarView = 'month';
@@ -17,10 +17,67 @@ angular.module('pilotPresenceApp')
       {
         title: 'My event title', // The title of the event
         type: 'info', // The type of the event (determines its color). Can be important, warning, info, inverse, success or special
-        starts_at: new Date(2013,5,1,1), // A javascript date object for when the event starts
-        ends_at: new Date(2014,8,26,15), // A javascript date object for when the event ends
+        starts_at: new Date(2015,2,1,1), // jshint ignore:line
+        // A javascript date object for when the event starts
+        ends_at: new Date(2015,2,1,15), // jshint ignore:line
+        // A javascript date object for when the event ends
         editable: false, // If calendar-edit-event-html is set and this field is explicitly set to false then dont make it editable
         deletable: false // If calendar-delete-event-html is set and this field is explicitly set to false then dont make it deleteable
+      },
+      {
+        "title": "Event 1",
+        "type": "warning",
+        "starts_at": "2015-03-25T04:30:00.000Z",
+        "ends_at": "2015-03-25T09:30:00.000Z",
+        "$id": 0,
+        "daySpan": 1,
+        "dayOffset": 3,
+        "top": 0,
+        "height": 210,
+        "left": 0
       }
     ];
+    $scope.calendarClicked = function($event){
+      console.log('calendar clicked', $event);
+    };
+
+    var directiveScope;
+    $scope.$watch(
+      function watchedExpression(){
+        var monthCalendarEl = document.querySelector('#monthCalendar mwl-calendar-month');
+        return angular.element(monthCalendarEl).isolateScope();
+
+      }, function watchHandler(isolatedScope){
+        if(!isolatedScope) {
+          return;
+        }
+
+        directiveScope = isolatedScope;
+        console.log('month calendar scope');
+        console.dir(directiveScope);
+        directiveScope.dayClicked = dayClicked;
+        directiveScope.drillDown = null;
+        var dateStr = $stateParams.year + '-' + $stateParams.month + '-' + $stateParams.day;
+        directiveScope.selectedDay = moment(dateStr);
+        console.log('state params' ,$stateParams);
+      }
+    );
+
+    function dayClicked(rowIndex, cellIndex){
+      var day = directiveScope.view[rowIndex][cellIndex].date.startOf('day').toDate();
+      var params = {
+        year: $filter('date')(day, 'yyyy'),
+        month: $filter('date')(day, 'MM'),
+        day: $filter('date')(day, 'dd')
+      };
+      console.log('view', day);
+
+      if($state.current.name !== 'booking.day') {
+        $state.go('booking.day', params);
+        return;
+      }
+      $state.go('booking.day', params, {location: false});
+    }
+
+
   });
--- app/scripts/controllers/day.js
+++ app/scripts/controllers/day.js
@@ -8,19 +8,66 @@
  * Controller of the pilotPresenceApp
  */
 angular.module('pilotPresenceApp')
-  .controller('DayCtrl', function ($scope, ratioClass) {
+  .controller('DayCtrl', function ($scope, ratioClass, $stateParams) {
     $scope.$emit('dayClass', ratioClass);
 
     $scope.calendarView = 'day';
-    $scope.calendarDay = new Date();
+    $scope.calendarDay = new Date($stateParams.year + '-' + $stateParams.month + '-' + $stateParams.day);
+    console.log($scope.calendarDay);
     $scope.events = [
       {
         title: 'My event title', // The title of the event
         type: 'info', // The type of the event (determines its color). Can be important, warning, info, inverse, success or special
-        starts_at: new Date(2013,5,1,1), // A javascript date object for when the event starts
-        ends_at: new Date(2014,8,26,15), // A javascript date object for when the event ends
+        starts_at: new Date(2015,2,1,1), // jshint ignore:line
+        // A javascript date object for when the event starts
+        ends_at: new Date(2015,2,1,15), // jshint ignore:line
+        // A javascript date object for when the event ends
         editable: false, // If calendar-edit-event-html is set and this field is explicitly set to false then dont make it editable
         deletable: false // If calendar-delete-event-html is set and this field is explicitly set to false then dont make it deleteable
+      },
+      {
+        "title": "Sherlock",
+        "type": "warning",
+        "starts_at": "2015-03-25T04:30:00.000Z",
+        "ends_at": "2015-03-25T09:30:00.000Z",
+        "$id": 0,
+        "daySpan": 1,
+        "dayOffset": 3,
+        "top": 0,
+        "height": 210,
+        "left": 0
+      },
+      {
+        "title": "MyCraft",
+        "starts_at": "2015-03-25T04:30:00.000Z",
+        "ends_at": "2015-03-25T09:30:00.000Z",
+        "$id": 0,
+        "daySpan": 1,
+        "dayOffset": 3,
+        "top": 0,
+        "height": 210,
+        "left": 0
       }
     ];
+
+    var directiveScope;
+    $scope.$watch(
+      function watchedExpression(){
+        var monthCalendarEl = document.querySelector('#dayCalendar mwl-calendar-day');
+        return angular.element(monthCalendarEl).isolateScope();
+
+      }, function watchHandler(isolatedScope){
+        if(!isolatedScope) {
+          return;
+        }
+
+        directiveScope = isolatedScope;
+        console.log('day calendar scope');
+        console.dir(directiveScope);
+        //directiveScope.dayClicked = dayClicked;
+        //directiveScope.drillDown = null;
+        //directiveScope.selectedDay = moment(dateStr);
+        //console.log('state params' ,$stateParams);
+      }
+    );
   });
